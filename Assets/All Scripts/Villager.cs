63eabbc71e5023996074b9c9ee02ca6a77d47a6b
joshkosof <joshkosof@aol.com>
2016-01-30 17:18:05 -0500

    villager movement

--- Assets/All Scripts/Villager.cs
+++ Assets/All Scripts/Villager.cs
@@ -4,15 +4,25 @@ using System.Collections;
 public class Villager : NonPlayerCharacter {
 
 		int sacrificeValue;
+	Vector2 target;
+	int refresh = 0;
+
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		refresh--;
+		if (refresh == 0) {
+			target.x = Random.Range ((this.transform.localPosition.x - 50), (this.transform.localPosition.x + 50));
+			target.y = Random.Range ((this.transform.localPosition.y - 50), (this.transform.localPosition.y + 50));
+			refresh = 300;
+		}
+
+		Move ();
 	}
 
     override public bool sameTeam(Character c) {
