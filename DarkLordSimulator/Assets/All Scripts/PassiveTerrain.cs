5e5d024193dfdc0f47d2fd1469ba1aeb243139c3
SteveKrueger52 <stevekrueger52@gmail.com>
2016-01-30 11:25:06 -0500

    Project uploaded

---
+++ DarkLordSimulator/Assets/All Scripts/PassiveTerrain.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PassiveTerrain : WorldTerrain {
+
+		bool lethal; // will minions die if they touch this?
+
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+}
---
+++ DarkLordSimulator/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			GetComponent<Rigidbody2D>().transform.Translate(new Vector2(0, .1f));
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			GetComponent<Rigidbody2D>().transform.Translate(new Vector2(0, -0.1f));
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			GetComponent<Rigidbody2D>().transform.Translate(new Vector2(0.1f, 0));
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			GetComponent<Rigidbody2D>().transform.Translate(new Vector2(-0.1f, 0));
+		}
+	
+	}
+}
