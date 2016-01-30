ea76b1d37982f17a23b0a4cde24094d1568156b9
joshkosof <joshkosof@aol.com>
2016-01-30 14:40:06 -0500

    AI minion movement should work

--- Assets/All Scripts/Minion.cs
+++ Assets/All Scripts/Minion.cs
@@ -8,12 +8,33 @@ public class Minion : NonPlayerCharacter {
 		ArrayList holds; // All Villagers being held
 		int holdCapacity;
 		Vector2 target;
+	NonPlayerCharacter temp;
 	int range;
 	int range2;
 
 	// Use this for initialization
 	void Start () {
-	
+
+	}
+
+	public bool GetClosestEnemy(){
+		
+		foreach (OurGameObject m in Game.getObjectsWithin(this, range)) {
+			temp = this.leader;
+			if (m is NonPlayerCharacter) {
+				if (m is Minion) {
+					if ((this.team != (Minion)m.team) && distanceTo (m) < distanceTo (temp)) {
+						temp = m;
+					}
+				} else if (distanceTo (m) < distanceTo (temp)) {
+					temp = m;
+				}
+			}
+		}
+		if (!(temp is this.leader)){
+			return true;
+		} else return false;
+
 	}
 	
 	// Update is called once per frame
@@ -22,13 +43,14 @@ public class Minion : NonPlayerCharacter {
 	//get behavior
 		if (distanceTo(leader).magnitude > range2){
 			target = leader.refPos;
-		} else if (closest enemy within range){
-			//set target to enemy
-		} else if (closest Villager within range){
-			//set target to villager
+		} else if (GetClosestEnemy()){
+			target.x = temp.transform.localPosition.x;
+			target.y = temp.transform.localPosition.y;
 		} else {
 			target = leader.refpos;}
 
-		Move ();
+		this.acceleration.x = target.x - this.transform.localPosition.x;
+		this.acceleration.y = target.y - this.transform.localPosition.y;
+		Move();
 	}
 }
