ede1a630675d39739867f4df8903f5e8cb3acafc
SteveKrueger52 <stevekrueger52@gmail.com>
2016-01-30 13:16:39 -0500

    Vector2 vs Vector3

--- DarkLordSimulator/Assets/All Scripts/OurGameObject.cs
+++ DarkLordSimulator/Assets/All Scripts/OurGameObject.cs
@@ -6,8 +6,8 @@ public abstract class OurGameObject : MonoBehaviour {
 	// this.transform gets the Transform of the object, which includes location and scale
 	public bool isBlocking;
 
-    public Vector3 distanceTo(OurGameObject other) {
-        return new Vector3 (
+    public Vector2 distanceTo(OurGameObject other) {
+        return new Vector2 (
             other.transform.localPosition.x - this.transform.localPosition.x,
             other.transform.localPosition.y - this.transform.localPosition.y);
     }
