512f86b743a77b2c33e0f68408479305c8e59957
SteveKrueger52 <stevekrueger52@gmail.com>
2016-01-30 12:01:18 -0500

    More stuff

--- DarkLordSimulator/Assets/All Scripts/Character.cs
+++ DarkLordSimulator/Assets/All Scripts/Character.cs
@@ -3,7 +3,19 @@ using System.Collections;
 
 public abstract class Character : OurGameObject {
 
-		public int hp;
-		private int maxHP;
-	  	private float regen; // we should figure out how this works
+	public int hp;
+	private int maxHP;
+	private float regen; // we should figure out how this works
+    private int damageValue;
+
+    void attack(Character enemy) {
+        enemy.hp -= damageValue;
+        if (enemy.hp < 0) {
+            enemy.die ();
+        }
+    }
+
+    void die() {
+        //TODO
+    }
 }
