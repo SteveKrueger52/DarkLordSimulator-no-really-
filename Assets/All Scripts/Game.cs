266d9cffaf45d583362f924c34bfac8cc3bfb8f2
SteveKrueger52 <stevekrueger52@gmail.com>
2016-01-30 21:10:17 -0500

    added Initialization for the world

--- Assets/All Scripts/Game.cs
+++ Assets/All Scripts/Game.cs
@@ -5,9 +5,37 @@ public class Game : MonoBehaviour {
 
     ArrayList gameObjects;
 
+    [SerializeField] uint playerCount;
+
+    private Player[] players;
+
+    [SerializeField] Player player1;
+    [SerializeField] Player player2;
+    [SerializeField] Player player3;
+    [SerializeField] Player player4;
+
+    [SerializeField] Map Arena;
+
 	// Use this for initialization
 	void Start () {
-	
+        //Initialize players
+        players = new Player [playerCount];
+        players [0] = player1;
+        gameObjects.Add (player1);
+        players [1] = player2;
+        gameObjects.Add (player2);
+        if (playerCount > 2) {
+            players [2] = player3;
+            gameObjects.Add (player3);
+        }
+        if (playerCount > 3) {
+            players [3] = player4;
+            gameObjects.Add (player4);
+        }
+        //Initialize Terrain from Map PreFab
+        foreach (Terrain obj in Arena) {
+            gameObjects.Add (obj);
+        }
 	}
 	
 	// Update is called once per frame
