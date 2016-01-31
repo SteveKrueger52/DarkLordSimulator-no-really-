ae9a8be371aff98179177d38e546a80f40000a5d
barrettand <barrett.and@husky.neu.edu>
2016-01-31 07:11:07 -0500

    who even reads this

--- Assets/All Scripts/Team.cs
+++ Assets/All Scripts/Team.cs
@@ -4,19 +4,24 @@ using System.Collections;
 public class Team : MonoBehaviour {
 
 	public ArrayList effects; // references to all active effects on this team
-	public ArrayList players; // references to all the players in the team
-	private int teamID;
+	public Player player; // references to all the players in the team
+
     public UnityEngine.Color teamColor;
-    public int score;
+    public int score = 0;
+
+	[SerializeField] public int teamID;
+	[SerializeField] public TextMesh playerDisplay;
+	[SerializeField] public TextMesh scoreDisplay;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		playerDisplay.text = "Player: " + teamID.ToString();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		scoreDisplay.text = "Score: " + score.ToString ();
 	}
 
     public void addScore(int score) {
